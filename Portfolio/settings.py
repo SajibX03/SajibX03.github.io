d15d31ba267bfea198a4607c2a12d30a97b4d062
Sajib Baran Shyamal <143562190+SajibX03@users.noreply.github.com>
2024-02-03 13:57:16 +0530

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -78,7 +78,20 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
 # To use Neon with Django, you have to create a Project on Neon and specify the project connection settings in your settings.py in the same way as for standalone Postgres.
 
-DATABASES = {}
+DATABASES = {# To use Neon with Django, you have to create a Project on Neon and specify the project connection settings in your settings.py in the same way as for standalone Postgres.
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'dip',
+    'USER': 'SajibX03',
+    'PASSWORD': 'fsx3bdOKDBo5',
+    'HOST': 'ep-quiet-butterfly-a594facu-pooler.us-east-2.aws.neon.tech',
+    'PORT': '5432',
+    'OPTIONS': {'sslmode': 'require'},
+    'DISABLE_SERVER_SIDE_CURSORS': True,
+  }
+}}
 
 
 # Password validation
